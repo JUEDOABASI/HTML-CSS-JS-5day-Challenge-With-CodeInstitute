9ba6f6e9100f86e3eab22ce6c20588bf361cbe73
JUEDOABASI <157640466+JUEDOABASI@users.noreply.github.com>
2024-05-01 17:05:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+/*The script helps to change the ICON Size when the Mouse in In or Out of the Font-Awesome-ICON*/
+
+/*Script for th Ingredients*/
+function ingredientsHover(){
+    document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "300%"; /*When mouse touch the ICON*/
+}
+
+function ingredientsNormal(){
+    document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "100%"; /*When mouse IS OUT OF the ICON*/
+}
+
+
+/*Script for th Preparations*/
+function preparationsHover(){
+    document.getElementById('preparations').firstElementChild.firstElementChild.style.fontSize = "300%"; /*When mouse touch the ICON*/
+}
+
+function preparationsNormal(){
+    document.getElementById('preparations').firstElementChild.firstElementChild.style.fontSize = "100%"; /*When mouse IS OUT OF the ICON*/
+}
